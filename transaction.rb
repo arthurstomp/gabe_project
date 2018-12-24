246048eb90da48aac585c57d5e987e0c4a0e3919
Gabriel Cardoso <cardoso_49@hotmail.com>
2018-12-24 17:17:41 -0300

    Transactions JSON implemented

--- transaction.rb
+++ transaction.rb
@@ -1,16 +1,48 @@
+require "json"
 class Transaction
+  FilePath = "./transactions.json"
   attr_reader :product, :amount, :price
   @@transactions = []
-
-  def self.transactions
-    @@transactions
-  end
 
   def initialize(product, amount, price)
     set_product(product)
     @amount = amount
     @price = price
-    @@transactions.push(self)
+    # @@transactions.push(self)
+  end
+
+  def add(t)
+    @@transactions.push(t)
+  end
+
+  def self._prepare_transactions
+    JSON.parse(File.read(FilePath)).map do |t|
+      h = parse(t)
+      new t["product"], t["amount"], t["price"]
+    end
+  end
+
+  @@transactions = _prepare_transactions
+
+  def self.transactions
+    @@transactions
+  end
+
+  def self.save
+    hashed_transactions = @@transactions.map do |t|
+      JSON.generate(p.hashed_attrs)
+    end
+    File.open(FilePath, 'w') do |f|
+      f.write hashed_transactions
+    end
+  end
+
+  def hashed_attrs
+    {
+      product: @product,
+      amount: @amount,
+      price: @price
+    }
   end
 
   def self.stock(prod_name)
