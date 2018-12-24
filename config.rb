34d76e1f027ae1090adc7587e51333bc81dd6da1
Gabriel Cardoso <cardoso_49@hotmail.com>
2018-12-24 15:29:01 -0300

    Wip Project Class

--- config.rb
+++ config.rb
@@ -10,13 +10,25 @@ class Project
 
   def new_product(name, price)
     # TODO
+    Product.new(name, price)
   end
 
-  def buy_product(prod, amount, total_paid)
+  def buy_product(prod_name, amount, total_paid)
     # TODO
+    Transaction.new(search_product_by_name(prod_name), amount, total_paid)
   end
 
-  def sell_product(prod, amount, total_price)
+  def sell_product(prod_name, amount, total_price)
     # TODO
+    Transaction.new(search_product_by_name(prod_name), amount, total_paid)
+  end
+
+  def search_product_by_name(name)
+    x = nil
+    Product.products.each do |p|
+      if p.name == name
+        x = p
+      end
+    end
   end
 end
